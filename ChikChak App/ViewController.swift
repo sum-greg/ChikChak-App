7b5c5695762f95f70d3367c9f5298b66a8986b9c
sum-greg <grishasumlinsky@gmail.com>
2023-01-31 14:06:58 +0200

    Initial Commit

---
+++ ChikChak App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChikChak App
+//
+//  Created by Григорий Сумлинский on 31.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
